cd187018c6db4083b74f9cdd7b6645ced7f0331c
Jehru <jehru.e.harris@gmail.com>
2025-11-25 17:37:21 +1100

    Add npm lockfile

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+export type Layer = {
+  id: string;
+  name: string;
+  color: string;
+  visible: boolean;
+};
+
+export type NodeItem = {
+  id: string;
+  layerId: string;
+  x: number;
+  y: number;
+  text: string;
+};
+
+export type Connection = {
+  id: string;
+  fromNodeId: string;
+  toNodeId: string;
+  label?: string;
+};
+
+export type Viewport = {
+  scale: number;
+  offsetX: number;
+  offsetY: number;
+};
+
+export type DiagramState = {
+  layers: Layer[];
+  nodes: NodeItem[];
+  connections: Connection[];
+  viewport: Viewport;
+};
